b5807f0a29af190623486fa5550351c5e844fd3e
I440576 <n.duzanson@student.fontys.nl>
2022-04-28 11:58:52 +0200

    updated LogModel

--- LOUPE_Backend/LogHandler.Microservice/Model/LogModel.cs
+++ LOUPE_Backend/LogHandler.Microservice/Model/LogModel.cs
@@ -6,8 +6,8 @@ namespace LogHandler.Microservice.Model
     {
         [Key]
         public string logId { get; set; }
-        //[ForeignKey("User")]
         public string userId { get; set; }
         public string log { get; set; }
+        public DateTime created { get; set; }
     }
 }
