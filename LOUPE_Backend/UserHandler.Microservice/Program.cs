db09e89aad304c4e361869468af836cd5282cb5c
Nahir Duzanson <440576@student.fontys.nl>
2022-05-24 14:28:52 +0200

    implemented Jwt token generate

--- LOUPE_Backend/UserHandler.Microservice/Program.cs
+++ LOUPE_Backend/UserHandler.Microservice/Program.cs
@@ -5,6 +5,9 @@ using User.Microservice.Data;
 using User.Microservice.Model;
 using System.IdentityModel.Tokens.Jwt;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 var authenticationProviderKey = "UserKey";
@@ -15,9 +18,20 @@ builder.Services.AddScoped<IUserDAL, UserDAL>();
 builder.Services.AddDbContext<UserDbContext>(x => x.UseSqlServer(connectionString));
 
 builder.Services.AddAuthentication()
-    .AddJwtBearer(authenticationProviderKey, x =>
+    .AddJwtBearer(authenticationProviderKey, options =>
     {
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Secret"])),
+            ValidAudience = "apiUsers",
+            ValidIssuer = "apiIssuer",
+            ValidateIssuerSigningKey = true,
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.Zero
+        };
     });
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -64,6 +78,36 @@ app.MapPost("/user/add", ([FromServices] IUserDAL db, UserModel user) =>
 {
     db.AddUser(user);
 });
+
+app.MapPost("/user/jwt/login", (UserModel user) =>
+{
+    return GenerateToken(user);
+});
+
+IResult GenerateToken(UserModel user)
+{
+    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Secret"]));
+    var credentials =new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+    var expirationDate = DateTime.Now.AddHours(2);
+
+    var claims = new[]
+    {
+        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+        new Claim(JwtRegisteredClaimNames.Name, user.name),
+        new Claim("Role", "Admin")
+    };
+
+    var token = new JwtSecurityToken(audience: "apiUsers",
+                                        issuer: "apiIssuer",
+                                        claims: claims,
+                                        expires: expirationDate,
+                                        signingCredentials: credentials);
+
+    var authToken = new JwtSecurityTokenHandler().WriteToken(token);
+
+    return Results.Ok(authToken);
+
+}
 
 app.Run();
 
