916ca3a3887358acbe4750377debdb0bb320da7a
Jensevent <j.heesb1@gmail.com>
2022-06-16 14:29:30 +0200

    did migration on log

--- LOUPE_Backend/LogHandler.Microservice/Migrations/20220616120123_initialdb.cs
+++ LOUPE_Backend/LogHandler.Microservice/Migrations/20220616120123_initialdb.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace LogHandler.Microservice.Migrations
 {
-    public partial class initialDb : Migration
+    public partial class initialdb : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
@@ -15,7 +16,8 @@ namespace LogHandler.Microservice.Migrations
                     logId = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
                     userId = table.Column<int>(type: "int", nullable: false),
-                    log = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                    log = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    created = table.Column<DateTime>(type: "datetime2", nullable: false)
                 },
                 constraints: table =>
                 {
