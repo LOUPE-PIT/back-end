eafb81ff286408738e0e9d52c3a5bfd296d1489d
Yarik <Yarik@hetblomhof.nl>
2023-03-23 13:30:54 +0100

    Basic setup

--- LOUPE_Backend/AuthenticationService.API/Controllers/AuthenticationController.cs
+++ LOUPE_Backend/AuthenticationService.API/Controllers/AuthenticationController.cs
@@ -1,7 +1,12 @@
-﻿namespace AuthenticationService.API
-{
-    public class AuthenticationController
-    {
+﻿using Microsoft.AspNetCore.Mvc;
 
+namespace AuthenticationService.API.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
     }
 }
