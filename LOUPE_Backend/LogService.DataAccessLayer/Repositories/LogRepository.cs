771b291b9baa1c2596802ff7838176557f9f5823
Ilias <i.elhammouti@student.fontys.nl>
2023-04-03 10:50:15 +0200

    Added database settings and docker setup to logservice api

--- LOUPE_Backend/LogService.DataAccessLayer/Repositories/LogRepository.cs
+++ LOUPE_Backend/LogService.DataAccessLayer/Repositories/LogRepository.cs
@@ -4,7 +4,7 @@ using LogService.DataAccessLayer.Models;
 
 namespace LogService.DataAccessLayer.Repositories;
 
-public class LogRepository
+public class LogRepository : ILogRepository
 {
     //Context
     private readonly LogDbContext _logDbContext;
