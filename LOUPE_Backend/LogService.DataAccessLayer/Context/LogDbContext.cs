e12cb2a19046213b7531e48cb120d33a6060cb71
Ilias <i.elhammouti@student.fontys.nl>
2023-04-04 10:14:56 +0200

    removed unused connectionstring in LogDbContext

--- LOUPE_Backend/LogService.DataAccessLayer/Context/LogDbContext.cs
+++ LOUPE_Backend/LogService.DataAccessLayer/Context/LogDbContext.cs
@@ -18,16 +18,4 @@ public class LogDbContext : DbContext
     }
 
     public DbSet<Log> Logs { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        var configuration = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json")
-            .Build();
-        
-        var connectionString = configuration.GetConnectionString("logDb");
-
-        optionsBuilder.UseSqlServer(connectionString);
-    }
 }
