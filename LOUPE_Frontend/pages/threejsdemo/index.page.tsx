a7c2ee30beadcf4f823a7dab293d36be01661640
Ilias <i.elhammouti@student.fontys.nl>
2023-04-24 10:53:37 +0200

    ThreeJS demo created

---
+++ LOUPE_Frontend/pages/threejsdemo/index.page.tsx
@@ -0,0 +1,31 @@
+import React, {useEffect} from 'react'
+import './code.css'
+import { Canvas, useThree} from '@react-three/fiber'
+import Duck from '../../components/3Dobjects/duck';
+import { OrbitControls } from '@react-three/drei';
+
+export { Page }
+
+function Controls() {
+    const {
+        camera,
+        gl: {domElement},
+    } = useThree();
+
+    return <OrbitControls args={[camera, domElement]} />;
+}
+
+function Page() {
+    return (
+        <>
+            <h1>ThreeJS Demo</h1>
+            <Canvas style={{"position": "absolute"}}>
+                <ambientLight />
+                <pointLight position={[5,5,5]} intensity={1} />
+                <pointLight position={[-3,-3,2]} intensity={1} />
+                <Controls />
+                <Duck />
+            </Canvas>
+        </>
+    )
+}
