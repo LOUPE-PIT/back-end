fa0ad714b28dcd99bfa0038b20253e92c273c102
Yarik <Yarik@hetblomhof.nl>
2023-06-05 10:01:15 +0200

    Added db migrations

--- LOUPE_Backend/UserService.API/Program.cs
+++ LOUPE_Backend/UserService.API/Program.cs
@@ -1,13 +1,26 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using UserService.DataLayer.Context;
+using UserService.DataLayer.Models;
+using UserService.DataLayer.Services;
 
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("AppDb");
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<UserDbContext>(x => x.UseSqlServer());
+builder.Services.AddTransient<IUserDAL, UserDAL>();
 
 var app = builder.Build();
+
+using (IServiceScope? scope = app.Services.CreateScope())
+{
+    UserDbContext UserDbContext = scope.ServiceProvider.GetRequiredService<UserDbContext>();
+    UserDbContext.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
